027acc3bc0ba2ee24ab80b2def3decee0841dce9
k2works <kakimomokuri@gmail.com>
2018-07-09 13:35:43 +0900

    実演 実装
    テストを作成する

--- demo/FizzBuzz.rb
+++ demo/FizzBuzz.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 class FizzBuzz
 end
+
+require 'minitest/autorun'
+class FizzBuzzSpec < Minitest::Spec
+  describe FizzBuzz do
+    it '3ならばFizzを返す' do
+      expect(3).must_equal 'Fizz'
+    end
+  end
+end
