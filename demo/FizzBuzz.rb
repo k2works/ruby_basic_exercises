bdaa282cc5db368232ec20e3d5e4492bca1a05d1
k2works <kakimomokuri@gmail.com>
2018-07-09 13:52:28 +0900

    実演 実装
    Fizz出力の仮実装

--- demo/FizzBuzz.rb
+++ demo/FizzBuzz.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 class FizzBuzz
+  def generate(number)
+    'Fizz'
+  end
 end
 
 require 'minitest/autorun'
 class FizzBuzzSpec < Minitest::Spec
   describe FizzBuzz do
-    it '3ならばFizzを返す' do
-      expect(3).must_equal 'Fizz'
+    describe '#generate' do
+      it '3ならばFizzを返す' do
+        fizz_buzz = FizzBuzz.new
+        expect(fizz_buzz.generate(3)).must_equal 'Fizz'
+      end
     end
   end
 end
