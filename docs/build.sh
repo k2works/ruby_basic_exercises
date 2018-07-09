0eef692ad3219237fa08a110ee3f524c25a96073
k2works <kakimomokuri@gmail.com>
2018-07-09 12:17:28 +0900

    Initial Setup

--- docs/build.sh
+++ docs/build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-rm -rf dist
-mkdir -p dist/overview/
-mkdir -p dist/excercies/
-mkdir -p dist/demonstration/
-asciidoctor -r asciidoctor-diagram src/index.adoc -D dist/
-asciidoctor -r asciidoctor-diagram src/overview/*.adoc -D dist/overview/
-asciidoctor -r asciidoctor-diagram src/excercies/*.adoc -D dist/excercies/
-asciidoctor -r asciidoctor-diagram src/demonstration/*.adoc -D dist/demonstration/
+rm index.html
+rm -rf overview/
+rm -rf excercies/
+rm -rf demonstration/
+mkdir -p overview/
+mkdir -p excercies/
+mkdir -p demonstration/
+asciidoctor -r asciidoctor-diagram src/index.adoc -D ./
+asciidoctor -r asciidoctor-diagram src/overview/*.adoc -D ./overview/
+asciidoctor -r asciidoctor-diagram src/excercies/*.adoc -D ./excercies/
+asciidoctor -r asciidoctor-diagram src/demonstration/*.adoc -D ./demonstration/
