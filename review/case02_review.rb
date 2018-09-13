3a58ae1933b9195ed5cd3131e6e042fab089a10e
k2works <kakimomokuri@gmail.com>
2018-09-13 20:11:18 +0900

    コードレビュー Case02
    ・メソッド分割

--- review/case02_review.rb
+++ review/case02_review.rb
@@ -1,52 +1,61 @@
 class Zaiko
     def self.execute
-        file_path1 = '20180701_INPUT.csv'
-        file_path2 = '20180701_OUTPUT.csv'
-        file_path3 = '20180701_INPUT.csv'
-        data=[]
-        shou=[]
-        su=[]
+        @file_path1 = '20180701_INPUT.csv'
+        @file_path2 = '20180701_OUTPUT.csv'
+        @data=[]
+        @shou=[]
+        @su=[]
 
-        File.open(file_path3, mode = "r") do |f|
-            i=0
-            f.gets
-            f.each_line{|line|
-                data[i]=line.chomp
-                i+=1
-            }
-        end
+        read
+        summary
+        write
+    end
 
-        i=0
-
-        data.each do | d |
-            data[i]=d.split(",")
-            i+=1
-        end
-
-        data.each do | a |
-            a[1]=a[1].to_i
-        end
-        i=0
-
-        data.each do | d |
-            c = shou.index(d[0])
-            if (c == nil)
-                shou << d[0]
-                su << d[1]
-            else
-                su[c]+=d[1]
-            end
-        end
-
-        File.open(file_path2, 'w') do |f|
+    private
+    def self.write
+        File.open(@file_path2, 'w') do |f|
             f.write("商品名,受入数")
             f.write("\n")
-            (shou.count).times do | a |
-                f.write(shou[a])
+            (@shou.count).times do |a|
+                f.write(@shou[a])
                 f.write(",")
-                f.write(su[a])
+                f.write(@su[a])
                 f.write("\n")
             end
+        end
+    end
+
+    def self.summary
+        i = 0
+        @data.each do |d|
+            @data[i] = d.split(",")
+            i += 1
+        end
+
+        @data.each do |a|
+            a[1] = a[1].to_i
+        end
+        i = 0
+
+        @data.each do |d|
+            c = @shou.index(d[0])
+            if (c == nil)
+                @shou << d[0]
+                @su << d[1]
+            else
+                @su[c] += d[1]
+            end
+        end
+    end
+
+    def self.read
+        File.open(@file_path1, mode = "r") do |f|
+            i = 0
+            f.gets
+            f.each_line {|line|
+                @data[i] = line.chomp
+                i += 1
+            }
         end
     end
 end
