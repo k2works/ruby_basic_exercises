6bf26c2a3278459b7db6effb20c2b59a57bba9dc
k2works <kakimomokuri@gmail.com>
2018-09-13 20:19:48 +0900

    コードレビュー Case02
    ・ファイル整形

--- review/case02_review.rb
+++ review/case02_review.rb
@@ -1,63 +1,66 @@
+# frozen_string_literal: true
+
 class Zaiko
-    def initialize(input,output)
-        @file_path1 = input
-        @file_path2 = output
-        @data=[]
-        @shou=[]
-        @su=[]
+  def initialize(input, output)
+    @file_path1 = input
+    @file_path2 = output
+    @data = []
+    @shou = []
+    @su = []
+  end
+
+  def create_summary_file
+    read
+    summary
+    write
+  end
+
+  private
+
+  def write
+    File.open(@file_path2, 'w') do |f|
+      f.write('商品名,受入数')
+      f.write("\n")
+      @shou.count.times do |a|
+        f.write(@shou[a])
+        f.write(',')
+        f.write(@su[a])
+        f.write("\n")
+      end
+    end
+  end
+
+  def summary
+    i = 0
+    @data.each do |d|
+      @data[i] = d.split(',')
+      i += 1
     end
 
-    def create_summary_file
-        read
-        summary
-        write
+    @data.each do |a|
+      a[1] = a[1].to_i
     end
+    i = 0
 
-    private
-    def write
-        File.open(@file_path2, 'w') do |f|
-            f.write("商品名,受入数")
-            f.write("\n")
-            (@shou.count).times do |a|
-                f.write(@shou[a])
-                f.write(",")
-                f.write(@su[a])
-                f.write("\n")
-            end
-        end
+    @data.each do |d|
+      c = @shou.index(d[0])
+      if c.nil?
+        @shou << d[0]
+        @su << d[1]
+      else
+        @su[c] += d[1]
+      end
     end
+  end
 
-    def summary
-        i = 0
-        @data.each do |d|
-            @data[i] = d.split(",")
-            i += 1
-        end
-
-        @data.each do |a|
-            a[1] = a[1].to_i
-        end
-        i = 0
-
-        @data.each do |d|
-            c = @shou.index(d[0])
-            if (c == nil)
-                @shou << d[0]
-                @su << d[1]
-            else
-                @su[c] += d[1]
-            end
-        end
+  def read
+    File.open(@file_path1, mode = 'r') do |f|
+      i = 0
+      f.gets
+      f.each_line do |line|
+        @data[i] = line.chomp
+        i += 1
+      end
     end
-
-    def read
-        File.open(@file_path1, mode = "r") do |f|
-            i = 0
-            f.gets
-            f.each_line {|line|
-                @data[i] = line.chomp
-                i += 1
-            }
-        end
-    end
+  end
 end
