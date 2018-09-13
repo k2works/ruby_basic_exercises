9c3c92394e60d3d91ea38fce207968c9bd124db7
k2works <kakimomokuri@gmail.com>
2018-09-14 08:49:22 +0900

    コードレビュー Case05
    ・CSV読み込み・書き込みメソッド仮実装

--- review/case05.rb
+++ review/case05.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
+require 'csv'
 
 class Stock
   def initialize(input_file)
     @input_file = input_file
+  end
+
+  def output_csv_file
+    data_list = CSV.read(@input_file)
+    CSV.open('20180731_OUTPUT.csv', 'w') do |output_line|
+      data_list.each do |input_line|
+        output_line << input_line
+      end
+    end
   end
 end
--- review/case05_spec.rb
+++ review/case05_spec.rb
@@ -5,17 +5,21 @@ require '../review/case05'
 
 class Uc05Spec < Minitest::Spec
   describe Stock do
-    it '仕入れ商品を入荷する' do
-      stock = Stock.new('20180701_INPUT.csv')
-      output = <<~EOS
-        商品名,受入数
-        商品A,6
-        商品B,6
-        商品C,10
-      EOS
-      expected_csv = output
-      actual_csv = File.open('20180731_OUTPUT.csv').read
-      assert_equal(expected_csv, actual_csv)
+    describe '仕入れ商品を入荷する' do
+      it 'CSVファイルを出力する' do
+        output = <<~EOS
+          商品名,受入数
+          商品A,6
+          商品B,6
+          商品C,10
+        EOS
+
+        stock = Stock.new('20180701_INPUT.csv')
+        stock.output_csv_file
+        expected_csv = output
+        actual_csv = File.open('20180731_OUTPUT.csv').read
+        assert_equal(expected_csv, actual_csv)
+      end
     end
   end
 end
