5260678abe4e1937ea5bb79bc93769942ca783cc
k2works <kakimomokuri@gmail.com>
2018-09-14 09:32:21 +0900

    コードレビュー Case05
    ・集計処理作成

--- review/case05.rb
+++ review/case05.rb
@@ -15,14 +15,16 @@ class Stock
     data_list.shift
     output_data_list = []
     data_list.each do |data|
-      data_hash = { shouhinnmei: data[0], ukeiresu: data[1] }
+      data_hash = { shouhinnmei: data[0], ukeiresu: data[1].to_i }
       output_data_list << data_hash
     end
 
+    merged_output_data_list = output_data_list.flatten.map(&:values).group_by(&:first).map { |id, items| { shouhinnmei: id, ukeiresu: items.map(&:last).inject(:+) } }
+
     CSV.open('20180731_OUTPUT.csv', 'w') do |output_line|
-      output_line << [shouhinnmei,ukeiresu]
-      output_data_list.each do |input_line|
-        output_line << [input_line[:shouhinnmei],input_line[:ukeiresu]]
+      output_line << [shouhinnmei, ukeiresu]
+      merged_output_data_list.each do |input_line|
+        output_line << [input_line[:shouhinnmei], input_line[:ukeiresu]]
       end
     end
   end
