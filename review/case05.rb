acfc51d4a0521b601cbaa83b156a4922a41c3abd
k2works <kakimomokuri@gmail.com>
2018-09-14 09:13:44 +0900

    コードレビュー Case05
    ・読み込んだ値をHashに変換

--- review/case05.rb
+++ review/case05.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'csv'
 
 class Stock
@@ -8,9 +9,20 @@ class Stock
 
   def output_csv_file
     data_list = CSV.read(@input_file)
+    header = data_list.first
+    shouhinnmei = header[0]
+    ukeiresu = header[1]
+    data_list.shift
+    output_data_list = []
+    data_list.each do |data|
+      data_hash = { shouhinnmei: data[0], ukeiresu: data[1] }
+      output_data_list << data_hash
+    end
+
     CSV.open('20180731_OUTPUT.csv', 'w') do |output_line|
-      data_list.each do |input_line|
-        output_line << input_line
+      output_line << [shouhinnmei,ukeiresu]
+      output_data_list.each do |input_line|
+        output_line << [input_line[:shouhinnmei],input_line[:ukeiresu]]
       end
     end
   end
